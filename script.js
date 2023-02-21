10c20c59c3b8c0e7c47cb29ce2f10d3e2a15ba50
harshs0891 <harshs0891@gmail.com>
2023-02-21 13:14:37 +0530

    updated the next image of website

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ let navbar = document.querySelector('.header .navbar');
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-}
+};
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+// -------swiper effect ----------
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,                //for infinte right/left scrolling for swiper
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
